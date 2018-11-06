70233d17e358f2a42ebf232f936232d12dc7e55a
kj1192 <94sjcjfja@naver.com>
2018-11-07 05:23:29 +0900

    try load wav files from local dir

---
+++ Code/Unity/merge_sound/Assets/muteAndSolo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class muteAndSolo : MonoBehaviour {
+    AudioSource myAudio;
+    public AudioSource otherAudio;
+    public Button buttonM;
+    public Button buttonS;
+
+
+    int counter = 0;
+    float vol, othervol;
+    private void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+    public void mute()
+    {
+        counter++;
+        
+        if (counter % 2 == 1)
+        {
+            vol = myAudio.volume;
+            myAudio.volume = -80;
+            buttonM.image.color = Color.red;
+        }
+        else
+        {
+            myAudio.volume = vol;
+            counter = 0;
+            buttonM.image.color = Color.white;
+        }
+
+    }
+    public void solo()
+    {
+        counter++;
+
+        if (counter % 2 == 1)
+        {
+            othervol = otherAudio.volume;
+            otherAudio.volume = -80;
+            buttonS.image.color = Color.yellow;
+        }
+        else
+        {
+            otherAudio.volume = othervol;
+            counter = 0;
+            buttonS.image.color = Color.white;
+        }
+
+    }
+}
