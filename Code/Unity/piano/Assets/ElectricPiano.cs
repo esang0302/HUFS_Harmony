e2b4e0faafab4cf4b66f06c704033771e930fbb1
yjinsol <yjs1236@naver.com>
2018-11-07 20:10:07 +0900

    playing piano with collision

---
+++ Code/Unity/piano/Assets/ElectricPiano.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricPiano : MonoBehaviour
+{
+    public AudioSource audio;
+    public AudioClip jinosl;
+    public AudioClip D;
+    public AudioClip E;
+    public AudioClip F;
+    public AudioClip G;
+    public AudioClip A;
+    public AudioClip B;
+    public GameObject object1;
+    public int IsChanged = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "target")
+        {
+            IsChanged = 1;
+            object1 = GameObject.Find("Tecla1__1");
+            //audio = object1.GetComponent<AudioSource>();
+            audio.clip = jinosl;
+            //audio.Play();
+            //object1 = audio;
+            /*audio.clip = C;
+            object1 = GameObject.Find("ID343 1");
+            audio = object1.GetComponent<AudioSource>();
+            audio.clip = D;*/
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Code/Unity/piano/Assets/SoundByContact.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundByContact : ElectricPiano {
+
+    public AudioSource ticksource;
+    public AudioClip EC;
+    public AudioClip CC;
+    public AudioClip OC;
+
+	// Use this for initialization
+	void Start () {
+        ticksource = GetComponent<AudioSource>(); 
+        
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "target")
+        {/*
+            if (IsChanged == 1)
+            {
+                ticksource.clip = EC;
+            }
+            else
+            {
+                ticksource.clip = CC;
+            }*/
+            //ticksource.Play();
+            //ticksource.PlayOneShot(collisionSound);
+        }
+    		
+	}
+
+    private void Update()
+    {
+        
+    }
+}
