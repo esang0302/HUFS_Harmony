e7928c49b83f53e981060b21245001de187d8cf0
Jungmineee <jme9708@naver.com>
2018-11-08 05:09:17 +0900

    changing the volume of floor tom based on values of FSR sensor

---
+++ Code/Arduino/HarmonyArdunity/Assets/col_sound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class col_sound : MonoBehaviour
+{
+
+    public AudioSource ticksource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ticksource = GetComponent<AudioSource>();
+        ticksource.Play();
+    }
+
+    
+
+    private void Update()
+    {
+
+    }
+}
