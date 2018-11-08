fe048a47bdd4b6dba00c7529ecf9c9fe77ff1ab7
yjinsol <yjs1236@naver.com>
2018-11-08 15:43:22 +0900

    final

--- Code/Unity/piano/Assets/SoundByContact.cs
+++ Code/Unity/piano/Assets/SoundByContact.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundByContact : ElectricPiano {
+public class SoundByContact : MonoBehaviour {
 
     public AudioSource ticksource;
-    public AudioClip EC;
-    public AudioClip CC;
-    public AudioClip OC;
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +15,8 @@ public class SoundByContact : ElectricPiano {
 	// Update is called once per frame
 	void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "target")
-        {/*
-            if (IsChanged == 1)
-            {
-                ticksource.clip = EC;
-            }
-            else
-            {
-                ticksource.clip = CC;
-            }*/
-            //ticksource.Play();
+        {
+            ticksource.Play();
             //ticksource.PlayOneShot(collisionSound);
         }
     		
