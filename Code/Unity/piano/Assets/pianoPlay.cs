106662a0fd95730b3557173d8c67f8080bb1bb8a
kj1192 <94sjcjfja@naver.com>
2018-11-13 03:52:20 +0900

    Change Sound Mapping

---
+++ Code/Unity/piano/Assets/pianoPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pianoPlay : MonoBehaviour {
+    AudioSource source;
+    
+	void Start () {
+        for (int i = 0; i < 36 ;i++)
+        {
+            source = transform.GetChild(i).GetComponent<AudioSource>();
+            source.clip = Resources.Load("ClassicPiano/"+(i+1).ToString()) as AudioClip;
+        }
+    }
+}
