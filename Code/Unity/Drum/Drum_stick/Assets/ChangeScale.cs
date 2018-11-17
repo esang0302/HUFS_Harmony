611d3534b4a247193dcb934704205200a431cebb
jinwookim <esang0302@hanmail.net>
2018-11-18 05:01:23 +0900

    implement a drum stick

---
+++ Code/Unity/Drum/Drum_stick/Assets/ChangeScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour {
+    //public GameObject objectToBig;
+	// Use this for initialization
+	void Start () {
+        Physics.IgnoreLayerCollision(0,3, true);
+    }
+    public void scaleUp()
+    {
+        
+        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        
+    }
+    public void scaleDown()
+    {
+
+        transform.localScale = new Vector3(0f, 0f, 0f);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Code/Unity/Drum/Drum_stick/Assets/GetStick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Leap.Unity.Attributes;
+using Leap;
+namespace Leap.Unity
+{
+
+    public class GetStick : MonoBehaviour
+    {
+        HandModel HandModels;
+        Hand leap_hand;
+        Hand hand;
+        public HandModelBase HandModel = null;
+        GameObject cylinder;
+        public GameObject objectToSpawn;
+        public Vector3 spawnLocation;
+        public void spawn()
+        {   
+           // Instantiate(objectToSpawn, spawnLocation, Quaternion.identity);
+            GameObject Cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            cylinder.transform.position = new Vector3(0,0,0);
+            
+        }
+    
+        // Use this for initialization
+        void Start()
+        {
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+       /* private Vector3 GetPalmPosition()
+        {
+            Vector3 from = palm.PalmPosition.ToVector3();
+            Vector3 Direction = palm.Direction.ToVector3();
+        }
+        */
+    }
+}
