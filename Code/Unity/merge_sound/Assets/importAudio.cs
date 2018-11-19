8e3049e2063367c2a58db53dc6536358ce1f8de8
kj1192 <94sjcjfja@naver.com>
2018-11-20 02:37:16 +0900

    Change way to load audio files

--- Code/Unity/merge_sound/Assets/importAudio.cs
+++ Code/Unity/merge_sound/Assets/importAudio.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+//#if UNITY_EDITOR
 using UnityEditor;
-
+//#endif
 [RequireComponent(typeof(AudioSource))]
 public class importAudio : MonoBehaviour {
-
+    GameObject t;
     AudioSource audio;
     AudioClip loadedClip;
     private string musicFile;
-
-
+//#if UNITY_EDITOR
     public void findFile () {
-        musicFile = EditorUtility.OpenFilePanel("Load files", "", "");
+
+     //   musicFile = EditorUtility.OpenFilePanel("Load files", "", "");
+        t = GameObject.Find("ttt");
+        t.transform.Rotate(0, 0, 90);
         StartCoroutine(loadAudio());
     }
-
+//#endif
     public IEnumerator loadAudio()
     {
         WWW audio_loader = new WWW("file:///"+musicFile);
         while (!audio_loader.isDone)
-
             Debug.Log(audio_loader.progress);
         yield return audio_loader;
         
