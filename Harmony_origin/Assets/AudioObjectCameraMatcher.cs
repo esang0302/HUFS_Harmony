c26c706c673ec2a1a4671f94b015ec343d89a459
seyong545 <seyong545@naver.com>
2018-11-22 14:52:54 +0900

    Upload integrity software and Class Diagram

---
+++ Harmony_origin/Assets/AudioObjectCameraMatcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioObjectCameraMatcher : MonoBehaviour {
+    AudioSource audioObject;
+	// Use this for initialization
+	void Start () {
+        foreach (AudioSource audioObject in (AudioSource[])GameObject.FindObjectsOfType(typeof(AudioSource)))
+        {
+            audioObject.gameObject.transform.position = Camera.main.transform.position;
+        }
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	    // Find all objects with name starting with "AudioObject"
+        // and set their position to main camera position
+        
+	}
+}
---
+++ Harmony_origin/Assets/SoundWithDelay.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundWithDelay : MonoBehaviour
+{
+    public AudioClip play;
+    AudioSource audioSource;
+    Collision collider;
+    float timeSpan;
+    float checkTime;
+    public ParticleSystem particles;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        timeSpan = 0.2f;
+        checkTime = 0.3f; //2sec
+    }
+    private void Update()
+    {
+        timeSpan += Time.deltaTime;
+    }
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(gameObject.name);
+            GameObject.Find("Main Camera").GetComponent<TextFileFinder_launch>().fileBrowserOpen(gameObject.name);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("target"))
+        {
+            if (timeSpan >= checkTime)
+            {
+                
+                audioSource.volume = collision.relativeVelocity.magnitude / 50;
+                Debug.Log(collision.relativeVelocity.magnitude);
+                audioSource.PlayOneShot(audioSource.clip,audioSource.volume);
+                //audioSource.PlayOneShot(play, 1);
+                Debug.Log("collision.relativeVelocity.magnitude: " + (collision.relativeVelocity.magnitude) / 100);
+                foreach (ContactPoint contact in collision.contacts)
+                {
+                    GameObject particle = Instantiate(particles.gameObject, contact.point, Quaternion.LookRotation(contact.normal));
+                    Destroy(particle, 1);
+                }
+
+            }
+
+            timeSpan = 0.1f;
+            //Debug.Log("collision.relativeVelocity.magnitude: " + collision.relativeVelocity.magnitude / 100);
+
+        }
+
+    }
+
+}
