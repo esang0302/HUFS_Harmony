11ce2b612fc5d6a3225b886fe66987e7a5f39478
Jungmineee <jme9708@naver.com>
2018-11-27 21:09:43 +0900

    Kick drum and Sustain Pedal

---
+++ Code/Arduino/HarmonyPedal/Assets/WRMHL/Scripts/readwrite.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is used to read the data coming from the device.
+public class readwrite : MonoBehaviour
+{
+
+    wrmhl myDevice = new wrmhl(); // wrmhl is the bridge beetwen your computer and hardware.
+
+    [Tooltip("SerialPort of your device.")]
+    public string portName = "COM6";
+
+    [Tooltip("Baudrate")]
+    public int baudRate = 250000;
+
+
+    [Tooltip("Timeout")]
+    public int ReadTimeout = 20;
+
+    [Tooltip("Something you want to send.")]
+    public string dataToSend = "Hello World!";
+
+    [Tooltip("QueueLenght")]
+    public int QueueLenght = 1;
+
+    void Start()
+    {
+        myDevice.set(portName, baudRate, ReadTimeout, QueueLenght); // This method set the communication with the following vars;
+                                                                    //                              Serial Port, Baud Rates, Read Timeout and QueueLenght.
+        myDevice.connect(); // This method open the Serial communication with the vars previously given.
+        myDevice.send(dataToSend); // Send data to the device using thread.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(myDevice.readQueue()); // myDevice.read() return the data coming from the device using thread.
+    }
+
+    void OnApplicationQuit()
+    { // close the Thread and Serial Port
+        myDevice.close();
+    }
+}
