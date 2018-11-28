5adab8263096fe53ee29502b05bbd36d7bd20872
kj1192 <94sjcjfja@naver.com>
2018-11-29 04:44:46 +0900

    Add synthesizer

--- Code/Unity/keyboard/Assets/KeyPress.cs
+++ Code/Unity/keyboard/Assets/KeyPress.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class KeyPress : MonoBehaviour {
     public float MaxZRotation = 5.0f;
     float ZRotationResetVelocity = - 0.3f;
@@ -13,6 +13,15 @@ public class KeyPress : MonoBehaviour {
     private float t;
     private AudioSource audio;
     private AudioClip clip;
+
+    private double increment;
+    private double frequencyFixed = 440;
+    private float frequency;
+    float synthVol;
+    public float gain;
+    private double phase;
+    private double sampling_frequency = 48000.0;
+
     private void Awake()
     {
         collided = false;
@@ -20,25 +29,21 @@ public class KeyPress : MonoBehaviour {
         initialPosition = transform.position;
     }
     void Start () {
-        
+        frequency = GameObject.Find("piano").GetComponent<Oscillator>().frequencies[Int32.Parse(gameObject.name)];
         currentResetPlayTime = 0;
         audio = gameObject.GetComponent<AudioSource>();
         clip = audio.clip;
         ResetPlayTime = 0.4f;
+        synthVol = 1f;
     }
 
     // Update is called once per frame
    void Update () {
         //return to origin state of piano
         currentResetPlayTime += Time.deltaTime;
+        //onPiano();
+        onSynth();
 
-        if (transform.rotation.eulerAngles.z > 1.5f && currentResetPlayTime >= ResetPlayTime && !collided)
-        {
-            Debug.Log("SOUND ON " + currentResetPlayTime);
-            audio.PlayOneShot(clip, 1f);
-            currentResetPlayTime = 0f;
-        }
-        
         if (transform.rotation.eulerAngles.z > 0.2f && !collided)
         {
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0, -0.2f));
@@ -51,7 +56,6 @@ public class KeyPress : MonoBehaviour {
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, MaxZRotation);
             GetComponent<Rigidbody>().angularDrag = 0;
             GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            //t = 0;
         }
         //Can't over the limit of origin state
         if (transform.rotation.eulerAngles.z > 350 || transform.rotation.eulerAngles.z < 0)
@@ -59,29 +63,84 @@ public class KeyPress : MonoBehaviour {
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
             GetComponent<Rigidbody>().angularDrag = 0;
             GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            Debug.Log("Right!");
-            Debug.Log(transform.rotation.eulerAngles.z);
         }
 
         transform.position = initialPosition;
         GetComponent<Rigidbody>().velocity = Vector3.zero;
-
         
     }
+    public void OnAudioFilterRead(float[] data, int channels)
+    {
+        increment = frequency * 2.0 * Mathf.PI / sampling_frequency;
+        for (int i = 0; i < data.Length; i += channels)
+        {
+            phase += increment;
+            data[i] = makeSinWave();
+
+            if (channels == 2)
+            {
+                data[i + 1] = data[i];
+            }
+            if (phase > (Mathf.PI * 2))
+            {
+                phase = 0.0;
+            }
+        }
+    }
+
+    void onSynth()
+    {
+        if (transform.rotation.eulerAngles.z > 1.5f)
+        {
+            audio.PlayOneShot(clip, 1f);
+            gain = 0.5f;
+        }
+        else
+            gain = 0;
+    }
+
+    void onPiano()
+    {
+        if (transform.rotation.eulerAngles.z > 1.5f && currentResetPlayTime >= ResetPlayTime && !collided)
+        {
+            audio.PlayOneShot(clip, 1f);
+            currentResetPlayTime = 0f;
+        }
+    }
+
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.tag == "target")
             collided = false;
+        
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "target")
             collided = true;
-        //if (GetComponent<Collider>().bounds.max.x > collision.contacts[0].point.x && GetComponent<Collider>().bounds.min.x < collision.contacts[0].point.x
-        //    && GetComponent<Collider>().bounds.min.y < collision.contacts[0].point.y && GetComponent<Collider>().bounds.max.y > collision.contacts[0].point.y)
-        //{
-        //}
+
+        
     }
 
+   
+
+
+    public float makeSinWave()
+    {
+        return (float)(gain * Mathf.Sin((float)phase));
+    }
+
+    public float makeSquareWave()
+    {
+        if (gain * Mathf.Sin((float)phase) >= 0 * gain)
+            return (float)gain * 0.6f;
+        else
+            return (-(float)gain) * 0.6f;
+    }
+
+    public float makeTriangleWave()
+    {
+        return (float)(gain * (double)Mathf.PingPong((float)phase, 1.0f));
+    }
 }
--- Code/Unity/keyboard/Assets/pianoPlay.cs
+++ Code/Unity/keyboard/Assets/pianoPlay.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class pianoPlay : MonoBehaviour {
     AudioSource source;
-    
+    private string inst;//classic, electric, synth
+    private string path;
 	void Awake () {
-        for (int i = 0; i < 36 ;i++)
+        classic();
+        //electric();
+    }
+
+    public void classic()
+    {
+        path = "ClassicPiano/";
+        selectSound(path);
+    }
+    public void electric()
+    {
+        path = "ElectricPiano/";
+        selectSound(path);
+    }
+    public void synth()
+    {
+
+    }
+
+    void selectSound(string path)
+    {
+        for (int i = 0; i < 36; i++)
         {
             source = transform.GetChild(i).GetComponent<AudioSource>();
-            source.clip = Resources.Load("ClassicPiano/"+(i+1).ToString()) as AudioClip;
+            source.clip = Resources.Load(path + (i + 1).ToString()) as AudioClip;
         }
     }
 
