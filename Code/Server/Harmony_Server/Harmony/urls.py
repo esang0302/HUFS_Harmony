d3a14a4f10da1a30699bc280219e5a56094862bc
Jungmineee <jme9708@naver.com>
2018-12-09 13:25:27 +0900

    audio uploading, Signup, Board

---
+++ Code/Server/Harmony_Server/Harmony/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'Harmony'
+
+urlpatterns = [
+    url(r'^$', views.showfile),
+    # /Harmony 으로 접속시 (include 됨)
+    # CBV의 generic view를 이용하여 url을 처리하겠다는 말
+]
